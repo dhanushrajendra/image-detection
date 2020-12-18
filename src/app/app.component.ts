15f47ae53f0efad5176192b0e362392d15ffd0d4
dhanushrajendra <dhanush@athena.edu>
2020-12-18 21:05:38 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { createWorker } from 'tesseract.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'image-conversion';
+  ocrResult = 'Recognizing...';
+
+  constructor() {
+    this.doOCR();
+  }
+
+  async doOCR() {
+    const worker = createWorker({
+      logger: m => console.log(m),
+    });
+    await worker.load();
+    await worker.loadLanguage('eng');
+    await worker.initialize('eng');
+    const { data: { text } } = await worker.recognize('https://jeroen.github.io/images/testocr.png');
+    this.ocrResult = text;
+    console.log(text);
+    await worker.terminate();
+  }
 }
